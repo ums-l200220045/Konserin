1ccd959ab94483369a0f8407b3405fc0351f4415
Ashaf <l200220045@student.ums.ac.id>
2025-06-08 21:03:29 +0700

    fix: correct typo in forceScheme https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if ($this->app->environment('production')) {
-            URL::forceScheme('httpss');
+            URL::forceScheme('https');
         }
     }
 }
